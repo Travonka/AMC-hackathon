70d83a6e9a9850880ae6dfe40037815c153f3131
Travonka <travnikov2014@yandex.ru>
2020-09-05 14:30:45 +0300

    GetDataAdded

---
+++ GetData/ITransport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetData
+{
+    interface ITransport
+    {
+        public float Longitude { get; set; }
+
+        public float Latitude { get; set; }
+
+        public enum TransportType { }
+
+
+    }
+}
---
+++ GetData/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Net;
+namespace yandextransportapi
+{
+    class Program
+    {
+        WebRequest request = WebRequest.Create("");
+    }
+}
